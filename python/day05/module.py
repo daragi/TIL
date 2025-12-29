32216defed0d5636e54081581bc375e150501a49
daragi <kmfm11m@naver.com>
2025-12-29 17:16:31 +0900

    python study

--- python/day05/module.py
+++ python/day05/module.py
@@ -3,21 +3,21 @@
 
 # 기본 사용법
 # import 모듈 -> 모듈을 불러와서 사용할 수 있다.
-# import module_A
+import module_A
 
-# print(module_A.add(1,2)) # 호출 (call)
-# print(module_A.subtract(1,10000)) # 호출 
+print(module_A.add(1,2)) # 호출 (call)
+print(module_A.subtract(1,10000)) # 호출 
 
-# # 일부만 불러오고 싶을 때는?
-# from module_A import subtract
-# # from module_A import * # 함수를 다 불러올 수도 있음
+# 일부만 불러오고 싶을 때는?
+from module_A import subtract
+# from module_A import * # 함수를 다 불러올 수도 있음
 
-# print(subtract(1, 10000))
+print(subtract(1, 10000))
 
-# # 함수 이름이 너무 길 때?
-# from module_A import subtract as sb
+# 함수 이름이 너무 길 때?
+from module_A import subtract as sb
 
-# print(sb(1, 10000))
+print(sb(1, 10000))
 
 
 # 파이썬 내장 모듈
